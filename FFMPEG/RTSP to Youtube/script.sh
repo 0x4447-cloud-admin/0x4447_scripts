509c66f05e596e15d1d5525951ee93d603f2e79c
Iv Simaku <ivsimaku@gmail.com>
2018-10-30 22:48:43 -0400

    Made the following changes: 1. Removed the OS check 2. Added a general command check, if it doesn't exist, it suggests to install and exits script 3. Set SavedPath to "/.0x4447" 4. Set to automatically try to read from SavedPath 5. Set to create file automatically if there were no previously saved credentials

--- FFMPEG/RTSP to Youtube/script.sh
+++ FFMPEG/RTSP to Youtube/script.sh
@@ -1,357 +1,196 @@
 #!/bin/bash
 
 #
-#	1.	Set variable if OSFound equal to 0.
+#	1.	Set variable with the path of Saved credentials file.
 #
-OSFound=0
+SavedPath="/.0x4447"
 
 #
-#	2.	Set Variable if user has savedfile (0 default).
+#	2.	Set Variable ContinueScript to initial value of 0.
 #
-SavedFile=0
+ContinueScript=0
 
-#
-#	3.	Get release info and grep for debian.
-#
-cat /etc/*release* 2>/dev/null | grep -i debian 2>/dev/null 1>/dev/null
 
 #
-#	4.	Check if debian was found in release file.
+#	3.	Check if ffmpeg binary is installed.
 #
-if [ $? == 0 ]; then
+command -v ffmpeg 1>/dev/null 2>&1;
 
-	#
-	#	1.	Since OS was found, set OSFound to 1.
-	#
-	OSFound=1;
-
-	#
-	#	2.	Check for installation of  ffmpeg.
-	#
-	dpkg -s ffmpeg 2>&1 | head -1 | grep "not installed" 1>/dev/null
-
-	#
-	#	3.	If ffmpeg is not installed, give ERROR,instructions, and exit script.
-	#
-	if [ $? == 0 ]; then
-		echo "ERROR: ffmpeg is not installed";
-		echo "Please install using the following command:";
-		echo "# apt-get install ffmpeg";
-		exit 1;
-	fi;
-
+#
+#	4.	If ffmpeg doesn't exist, show error and exit script with exit value of 1.
+#
+if [ $? == 1 ]; then
+    echo "ERROR: ffmpeg does not seem installed";
+    echo "Please refer to your OS manual to install ffmpeg package";
+    echo "Exiting!"
+    exit 1;
 fi;
 
 #
-#	8.	Get release info and grep for linux releases (fedora, centos, redhat).
+#	5.	Check if the Saved credentials file exists.
 #
-grep -Ei 'fedora|centos|redhat' /etc/*release* 1>/dev/null 2>&1
-
-#
-#	9.	Check if linux release was found in release file.
-#
-if [ $? == 0 ]; then
-
-	#
-	#	1.	Since OS was found, set OSFound to 1.
-	#
-	OSFound=1;
-
-	#
-	#	2.	Check for installation of  ffmpeg.
-	#
-	rpm -qa| grep ffmpeg 1>/dev/null 2>&1
-
-	#
-	#	3.	If ffmpeg is not installed, give ERROR,instructions, and exit script.
-	#
-	if [ ! $? == 0 ]; then
-		echo "ERROR: ffmpeg is not installed";
-		echo "Please install using the following command:";
-		echo "# yum install ffmpeg";
-		echo "(If the above doesn't work, please check repositories)"
-		exit 1;
-	fi;
-
+if [ -f "$SavedPath" ]; then
+    
+    #
+    #	1.	Get RTSPURL, YoutubeURL, and YoutubeKey from file.
+    #
+    RTSPURL=$(grep RTSPURL "$SavedPath" | cut -d '=' -f2 );
+    YoutubeURL=$(grep YoutubeURL "$SavedPath" | cut -d '=' -f2 );
+    YoutubeKey=$(grep YoutubeKey "$SavedPath" | cut -d '=' -f2 );
+    
+    #
+    #	2.	If any of the variables don't have value, request information.
+    #
+    if [ -z "$RTSPURL" ] || [ -z "$YoutubeURL" ] || [ -z "$YoutubeKey" ]; then
+        
+        echo "ERROR: File doesn't have the proper information...Continuing with request"
+        #
+        #	1.	Set ContinueScript to continue with requesting the information from user.
+        #
+        ContinueScript=1;
+        
+    fi;
+    
 fi;
 
 #
-#	10.	Check if any OS is found, if not ERROR and exit.
+#	6.	Check if Saved credentials doesn't exist or if it didn't have proper information.
 #
-if [ "$OSFound" == 0 ]; then
-	echo "ERROR: OS not found";
-	exit 1;
+if [ ! -f $SavedPath ] || [ $ContinueScript == "1" ]; then
+    
+    #
+    #	1.	Request the address of camera.
+    #
+    echo "Enter the address of camera: (rtsp://<user>:<password>@<address|IP>:<port#>/<rest of address>) ";
+    
+    #
+    #	2.	Take input.
+    #
+    read -r RTSPURL;
+    
+    #
+    #	3.	Check if there was any input, if not exit.
+    #
+    if [ -z "$RTSPURL" ]; then
+        
+        echo "ERROR: No RTSP URL was entered";
+        echo "Exiting!"
+        exit 1;
+        
+    fi;
+    
+    #
+    #	4.	Request the stream address of youtube.
+    #
+    echo "Enter the stream address of YouTube: (Example: rtmp://a.rtmp.youtube.com/live2) ";
+    
+    #
+    #	5.	Take input.
+    #
+    read -r YoutubeURL;
+    
+    #
+    #	6.	Check if there was any input, if not exit.
+    #
+    if [ -z "$YoutubeURL" ]; then
+        
+        echo "ERROR: No YOUTUBE URL was entered";
+        echo "Exiting!"
+        exit 1;
+        
+    fi;
+    
+    #
+    #	7.	Request the stream key for youtube.
+    #
+    echo "Enter the stream key for youtube: (####-####-####-####) ";
+    
+    #
+    #	8.	Take input.
+    #
+    read -r YoutubeKey;
+    
+    #
+    #	9.	Check if there was any input, if not exit.
+    #
+    if [ -z "$YoutubeKey" ]; then
+        
+        echo "ERROR: No YOUTUBE KEY was entered";
+        echo "Exiting!"
+        exit 1;
+        
+    fi;
+    
+    
+    #
+    #	10.	Create empty file in the directory provided in SavedPath.
+    #
+    touch "$SavedPath"
+    
+    #
+    #	10.	Check if file was created.
+    #
+    if [ ! -f "$SavedPath" ]; then
+        
+        echo "ERROR: File cannot be saved to $SavedPath...Starting streaming"
+        #
+        #	1.	Set ContinueScript back to 0 so it doesn't try to input the data.
+        #
+        ContinueScript=0;
+    fi;
+    
+    #
+    #	11.	Input entries to file provided.
+    #
+    if [ $ContinueScript == "1" ]; then
+        echo "RTSPURL=$RTSPURL" > "$SavedPath"
+        echo "YoutubeURL=$YoutubeURL" >> "$SavedPath"
+        echo "YoutubeKey=$YoutubeKey" >> "$SavedPath"
+        echo "Information Saved to $SavedPath"
+    fi;
+    
+    #
+    #	12.	End of if for getting the required information.
+    #
 fi;
 
 #
-#	11.	Ask if user has saved information from previous use.
-#
-echo "Do you have saved information from previous use of script? (Y/N) ";
-
-#
-#	12.	Take input.
-#
-read -r SavedInfoAns;
-
-#
-#	13.	Check if there was any input, if not exit.
-#
-if [ -z "$SavedInfoAns" ]; then
-
-	echo "ERROR: No answer was entered";
-
-	exit 1;
-
-fi;
-
-#
-#	14.	Change the answer to Upper Case.
-#
-SavedInfo=$(echo "$SavedInfoAns" | awk '{print toupper($0)}')
-
-#
-#	15.	Check if the answer was Y or Yes.
-#
-if [ "$SavedInfo" == "Y" ] || [ "$SavedInfo" == "YES" ]; then
-
-	#
-	#	1.	Ask for path of saved file.
-	#
-	echo "Please provide path of saved information file:";
-
-	#
-	#	2.	Take input.
-	#
-	read -r SavedPath;
-
-	#
-	#	3.	Check if there was any input, if not request info.
-	#
-	if [ -z "$SavedPath" ]; then
-
-		echo "ERROR: No PATH was entered";
-
-		SavedFile=0;
-		#
-		#	23.	Check if file exists, if not request info.
-		#
-
-	elif [ ! -f "$SavedPath" ]; then
-
-		echo "ERROR: File does not exist"
-
-		SavedFile=0;
-		#
-		#	24.	If file exists set Varialbe to 1 for existence.
-		#
-	else
-		SavedFile=1;
-	fi;
-fi;
-
-#
-#	16.	Check if SaveFile variable is 1 to pull data.
-#
-if [ $SavedFile == "1" ]; then
-
-	#
-	#	1.	Get RTSPURL, YoutubeURL, and YoutubeKey from file.
-	#
-	RTSPURL=$(grep RTSPURL "$SavedPath" | cut -d '=' -f2 );
-	YoutubeURL=$(grep YoutubeURL "$SavedPath" | cut -d '=' -f2 );
-	YoutubeKey=$(grep YoutubeKey "$SavedPath" | cut -d '=' -f2 );
-
-	#
-	#	2.	If any of the variables don't have value, request information.
-	#
-	if [ -z "$RTSPURL" ] || [ -z "$YoutubeURL" ] || [ -z "$YoutubeKey" ]; then
-
-		echo "ERROR: File doesn't have the proper information...Continuing with request"
-
-		SavedFile=0;
-
-	fi;
-
-fi;
-
-#
-#	17.	Check if SavedFile is 0, meaning data to be requested.
-#
-if [ "$SavedFile" == "0" ]; then
-
-	#
-	#	1.	Request the address of camera.
-	#
-	echo "Enter the address of camera: (rtsp://<user>:<password>@<address|IP>:<port#>/<rest of address>) ";
-
-	#
-	#	2.	Take input.
-	#
-	read -r RTSPURL;
-
-	#
-	#	3.	Check if there was any input, if not exit.
-	#
-	if [ -z "$RTSPURL" ]; then
-
-		echo "ERROR: No RTSP URL was entered";
-
-		exit 1;
-
-	fi;
-
-	#
-	#	4.	Request the stream address of youtube.
-	#
-	echo "Enter the stream address of YouTube: (Example: rtmp://a.rtmp.youtube.com/live2) ";
-
-	#
-	#	5.	Take input.
-	#
-	read -r YoutubeURL;
-
-	#
-	#	6.	Check if there was any input, if not exit.
-	#
-	if [ -z "$YoutubeURL" ]; then
-
-		echo "ERROR: No YOUTUBE URL was entered";
-
-		exit 1;
-
-	fi;
-
-	#
-	#	7.	Request the stream key for youtube.
-	#
-	echo "Enter the stream key for youtube: (####-####-####-####) ";
-
-	#
-	#	8.	Take input.
-	#
-	read -r YoutubeKey;
-
-	#
-	#	9.	Check if there was any input, if not exit.
-	#
-	if [ -z "$YoutubeKey" ]; then
-
-		echo "ERROR: No YOUTUBE KEY was entered";
-
-		exit 1;
-
-	fi;
-
-	#
-	#	10.	Ask if user wants to save information for future use.
-	#
-	echo "Would you like to save information for future use? (Y/N) ";
-
-	#
-	#	11.	Take input.
-	#
-	read -r SaveInfoAns;
-
-	#
-	#	12.	Check if there was any input, if yes make it upper case.
-	#
-	if [ ! -z "$SaveInfoAns" ]; then
-
-		SaveInfo=$(echo "$SaveInfoAns" | awk '{print toupper($0)}')
-
-	fi;
-
-	#
-	#	13.	Check if user entered Y or YES.
-	#
-	if [ "$SaveInfo" == "Y" ] || [ "$SaveInfo" == "YES" ]; then
-
-		#
-		#	1.	Ask user for path of where file is to be saved.
-		#
-		echo "Please provide path of where to save information file: (<Dir>/<FileName>)";
-
-		#
-		#	2.	Take input.
-		#
-		read -r SaveFile;
-
-		#
-		#	3.	Check if there was any input, if not continue without saving..
-		#
-		if [ -z "$SaveFile" ]; then
-
-			echo "ERROR: No PATH was entered";
-
-		else
-
-			#
-			#	1.	Create file if a path was entered.
-			#
-			touch "$SaveFile"
-
-		fi;
-
-		#
-		#	4.	Check if file exists, if not continue without saving.
-		#
-		if [ ! -f "$SaveFile" ]; then
-
-			echo "ERROR: File cannot be saved to $SaveFile...Starting streaming"
-
-			#
-			#	1.	Input entries to file provided.
-			#
-		else
-			echo "RTSPURL=$RTSPURL" > "$SaveFile"
-			echo "YoutubeURL=$YoutubeURL" >> "$SaveFile"
-			echo "YoutubeKey=$YoutubeKey" >> "$SaveFile"
-			echo "Information Saved to $SaveFile"
-		fi;
-	fi;
-
-	#
-	#	48.	End of if regarding saved file not existing.
-	#
-fi;
-
-#
-#	18.	Assign variable the flags for the FFMPEG Input.
+#	7.	Assign variable the flags for the FFMPEG Input.
 #
 FFMPEG_INPUT="-rtsp_transport tcp -thread_queue_size 512 -i ${RTSPURL}"
 
 #
-#	19.	Assign variable the flags for the FFMPEG Output.
+#	8.	Assign variable the flags for the FFMPEG Output.
 #
 FFMPEG_OUTPUT="-r 25 -tune zerolatency -pix_fmt + -vsync 1 -async 1 -c:v copy -c:a aac -b:v 400k -bufsize 512k -f flv ${YoutubeURL}/${YoutubeKey}"
 
 #
-#	20.	Create variable that will run the whole command without any visible errors.
+#	9.	Create variable that will run the whole command without any visible errors.
 #
 COMMAND="ffmpeg -hide_banner -nostdin -nostats ${FFMPEG_INPUT} ${FFMPEG_OUTPUT}"
 
 #
-#	21.	Check if a stream is already running.
+#	10.	Check if a stream is already running.
 #
-ps -ef | grep -i ffmpeg | grep -v grep | grep -i rtmp 1>/dev/null 2>&1;
+pgrep ffmpeg 1>/dev/null 2>&1;
 
 #
-#	22.	Check if a stream is already running, if Yes then kill.
+#	11.	Check if a stream is already running, if Yes then exit script with value 1.
 #
 if [ $? == "0" ]; then
-
-	echo "An ffmpeg instance is running, please stop other instances before running this one ";
-
-	exit 1;
-
+    
+    echo "ERROR: An ffmpeg instance is running, please stop other instances before running this one ";
+    echo "Exiting!"
+    exit 1;
+    
 fi;
 
 #
-#	23.	Inform User streaming has started.
+#	12.	Inform User streaming has started.
 #
 echo "Streaming started..."
 
 #
-#	24.	Run the COMMAND variable.
+#	13.	Run the COMMAND variable.
 #
-eval "$COMMAND" &> /dev/null &
+eval "$COMMAND"
