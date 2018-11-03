b63dcd213adc5b920b7e290e976ba6c78557e354
Iv Simaku <ivsimaku@gmail.com>
2018-11-03 19:20:27 -0400

    Modified script.sh for RTSP to Youtube: Set the saved credentials directory to home directory of user Fixed a script logic

--- FFMPEG/RTSP to Youtube/script.sh
+++ FFMPEG/RTSP to Youtube/script.sh
@@ -3,7 +3,7 @@
 #
 #	1.	Set variable with the path of Saved credentials file.
 #
-SavedPath="/.0x4447"
+SavedPath="$HOME/.0x4447"
 
 #
 #	2.	Set Variable ContinueScript to initial value of 0.
@@ -31,29 +31,29 @@ fi;
 #	5.	Check if the Saved credentials file exists.
 #
 if [ -f "$SavedPath" ]; then
-
+    
     #
     #	1.	Get RTSPURL, YoutubeURL, and YoutubeKey from file.
     #
     RTSPURL=$(grep RTSPURL "$SavedPath" | cut -d '=' -f2 );
     YoutubeURL=$(grep YoutubeURL "$SavedPath" | cut -d '=' -f2 );
     YoutubeKey=$(grep YoutubeKey "$SavedPath" | cut -d '=' -f2 );
-
+    
     #
     #	2.	If any of the variables don't have value, request information.
     #
     if [ -z "$RTSPURL" ] || [ -z "$YoutubeURL" ] || [ -z "$YoutubeKey" ]; then
-
+        
         echo "ERROR: File doesn't have the proper information...Continuing with request"
-
+        
         #
         #	1.	Set ContinueScript to continue with requesting the information
         #       from user.
         #
         ContinueScript=1;
-
+        
     fi;
-
+    
 fi;
 
 #
@@ -61,91 +61,96 @@ fi;
 #       information.
 #
 if [ ! -f $SavedPath ] || [ $ContinueScript == "1" ]; then
-
+    
     #
     #	1.	Request the address of camera.
     #
     echo "Enter the address of camera: (rtsp://<user>:<password>@<address|IP>:<port#>/<rest of address>) ";
-
+    
     #
     #	2.	Take input.
     #
     read -r RTSPURL;
-
+    
     #
     #	3.	Check if there was any input, if not exit.
     #
     if [ -z "$RTSPURL" ]; then
-
+        
         echo "ERROR: No RTSP URL was entered";
         echo "Exiting!"
         exit 1;
-
+        
     fi;
-
+    
     #
     #	4.	Request the stream address of youtube.
     #
     echo "Enter the stream address of YouTube: (Example: rtmp://a.rtmp.youtube.com/live2) ";
-
+    
     #
     #	5.	Take input.
     #
     read -r YoutubeURL;
-
+    
     #
     #	6.	Check if there was any input, if not exit.
     #
     if [ -z "$YoutubeURL" ]; then
-
+        
         echo "ERROR: No YOUTUBE URL was entered";
         echo "Exiting!"
         exit 1;
-
+        
     fi;
-
+    
     #
     #	7.	Request the stream key for youtube.
     #
     echo "Enter the stream key for youtube: (####-####-####-####) ";
-
+    
     #
     #	8.	Take input.
     #
     read -r YoutubeKey;
-
+    
     #
     #	9.	Check if there was any input, if not exit.
     #
     if [ -z "$YoutubeKey" ]; then
-
+        
         echo "ERROR: No YOUTUBE KEY was entered";
         echo "Exiting!"
         exit 1;
-
+        
     fi;
-
+    
     #
-    #	10.	Create empty file in the directory provided in SavedPath.
+    #	10.	Create empty file in the directory provided in SavedPath and set ContinueScript to 1.
     #
     touch "$SavedPath"
-
+    
     #
-    #	10.	Check if file was created.
+    #	11.	Make sure that ContinueScript is set to 1.
+    #
+    ContinueScript=1;
+    
+    #
+    #	12.	Check if file was created.
     #
     if [ ! -f "$SavedPath" ]; then
-
+        
         echo "ERROR: File cannot be saved to $SavedPath...Starting streaming"
-
+        
         #
         #	1.	Set ContinueScript back to 0 so it doesn't try to input the
         #       data.
         #
         ContinueScript=0;
     fi;
-
+    
     #
-    #	11.	Input entries to file provided.
+    #	13.	Input entries to file provided.
     #
     if [ $ContinueScript == "1" ]; then
         echo "RTSPURL=$RTSPURL" > "$SavedPath"
@@ -153,7 +158,7 @@ if [ ! -f $SavedPath ] || [ $ContinueScript == "1" ]; then
         echo "YoutubeKey=$YoutubeKey" >> "$SavedPath"
         echo "Information Saved to $SavedPath"
     fi;
-
+    
 fi;
 
 #
@@ -182,11 +187,11 @@ pgrep ffmpeg 1>/dev/null 2>&1;
 #       value 1.
 #
 if [ $? == "0" ]; then
-
+    
     echo "ERROR: An ffmpeg instance is running, please stop other instances before running this one ";
     echo "Exiting!"
     exit 1;
-
+    
 fi;
 
 #
